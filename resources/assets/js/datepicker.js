0866d2a57574fd909c5f2b7018ddfa58d5dce809
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-03-28 11:47:18 +0200

    Added personal form fields

--- resources/assets/js/datepicker.js
+++ resources/assets/js/datepicker.js
@@ -1,7 +1,6 @@
 import Datepicker from 'flowbite-datepicker/Datepicker';
 
 document.querySelectorAll('*[datepicker]').forEach((el) => {
-    console.log('Datepicker', el)
     new Datepicker(el, {
         format: 'dd. mm. yyyy',
     });
--- resources/views/livewire/member/profile/personal-form.blade.php
+++ resources/views/livewire/member/profile/personal-form.blade.php
@@ -54,9 +54,11 @@
     </div>
 
     <div>
-        <label for="displayname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Anzeigenamen</label>
-        <select id="displayname" @error('displayname') aria-describedby="displayname-error" @enderror
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <label for="displayname"
+               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Anzeigenamen</label>
+        <select id="displayname" name="displayname" wire:model="displayname"
+                @error('displayname') aria-describedby="displayname-error" @enderror
+                lass="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             <option value="first_name">Vorname</option>
             <option value="last_name">Nachname</option>
             <option value="fullname">Voller Name</option>
@@ -71,35 +73,94 @@
     </div>
 
     <div>
-        <div class="relative max-w-sm">
+        <label for="birthday" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Geburtsdatum</label>
+        <div class="relative w-full">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                          clip-rule="evenodd"></path>
+                </svg>
             </div>
-            <input datepicker type="text" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+            <input datepicker type="text" id="birthday" name="birthday" wire:model="birthday"
+                   class="w-full bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   placeholder="Datum auswählen"
+                   @error('birthday') aria-describedby="birthday-error" @enderror>
         </div>
 
-        @error('displayname')
-        <p id="displayname-error" class="mt-2 text-xs text-red-400 dark:text-red-600">
+        @error('birthday')
+        <p id="birthday-error" class="mt-2 text-xs text-red-400 dark:text-red-600">
             {{ $message }}
         </p>
         @enderror
     </div>
 
-    <button type="submit" wire:click.prevent="login" wire:loading.attr="disabled" wire:target="login"
-            class="w-full px-5 py-3 text-base font-medium text-center bg-accent-400 rounded-lg hover:bg-accent-600 focus:ring-4 focus:ring-accent-300 dark:bg-accent-700 dark:hover:bg-accent-800 dark:focus:ring-accent-800 disabled:cursor-not-allowed disabled:hover:bg-accent-400 disabled:dark:hover:bg-accent-600">
-        <span wire:loading.remove wire:target="login">Anmelden</span>
-        <span wire:loading wire:target="login">
-                <svg aria-hidden="true" role="status"
-                     class="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-accent-600 dark:fill-accent-400"
-                     viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"/>
-                    <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="fillColor"/>
-                </svg>
-                Lädt...
-            </span>
-    </button>
+    <div>
+        <div class="relative">
+            <input type="text" id="class" name="class" placeholder=" " wire:model="class"
+                   class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 peer focus:border-accent-400 dark:focus:border-accent-600 @error('class') !border-red-400 dark:!border-red-600 @enderror"
+                   @error('class') aria-describedby="class-error" @enderror/>
+            <label for="class"
+                   class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1 peer-focus:text-accent-400 peer-focus:dark:text-accent-600 @error('class') !text-red-400 dark:!text-red-600 @enderror">
+                Klasse
+            </label>
+        </div>
+        @error('class')
+        <p id="class-error" class="mt-2 text-xs text-red-400 dark:text-red-600">
+            {{ $message }}
+        </p>
+        @enderror
+    </div>
+
+    <div>
+        <label for="gender"
+               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Geschlecht</label>
+        <select id="gender" @error('gender') aria-describedby="gender-error" @enderror
+        class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="no">Nicht angegeben</option>
+            <option value="m">Männlich</option>
+            <option value="w">Weiblich</option>
+            <option value="o">Divers</option>
+        </select>
+
+        @error('gender')
+        <p id="gender-error" class="mt-2 text-xs text-red-400 dark:text-red-600">
+            {{ $message }}
+        </p>
+        @enderror
+    </div>
+
+    <div>
+        <div class="relative">
+            <input type="text" id="slogan" name="slogan" placeholder=" " wire:model="slogan"
+                   class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 peer focus:border-accent-400 dark:focus:border-accent-600 @error('slogan') !border-red-400 dark:!border-red-600 @enderror"
+                   @error('slogan') aria-describedby="slogan-error" @enderror/>
+            <label for="slogan"
+                   class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1 peer-focus:text-accent-400 peer-focus:dark:text-accent-600 @error('slogan') !text-red-400 dark:!text-red-600 @enderror">
+                Slogan
+            </label>
+        </div>
+        @error('slogan')
+        <p id="slogan-error" class="mt-2 text-xs text-red-400 dark:text-red-600">
+            {{ $message }}
+        </p>
+        @enderror
+    </div>
+
+    <div>
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="profile_picture">
+            Profilbild
+        </label>
+        <input
+            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            id="profile_picture" type="file"
+            @error('profile_picture') aria-describedby="profile_picture-error" @enderror>
+
+        @error('profile_picture')
+        <p id="profile_picture-error" class="mt-2 text-xs text-red-400 dark:text-red-600">
+            {{ $message }}
+        </p>
+        @enderror
+    </div>
 </form>
