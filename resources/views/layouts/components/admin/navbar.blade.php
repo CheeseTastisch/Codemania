f820b88e9d0580f8f7eede3cf0de4da4470ba4a2
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-04-11 21:19:47 +0200

    Added logo

--- resources/views/layouts/components/admin/navbar.blade.php
+++ resources/views/layouts/components/admin/navbar.blade.php
@@ -2,7 +2,8 @@
     class="bg-slate-200 dark:bg-slate-800 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-400 dark:border-gray-600">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
         <a href="{{ route('public.home') }}" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo">
+            <img src="{{ asset('storage/img/' . (theme()?->images ?? 'backup') . '/logo/dark-text.svg') }}" class="h-6 mr-3 sm:h-9 dark:hidden" alt="Codemania">
+            <img src="{{ asset('storage/img/' . (theme()?->images ?? 'backup') . '/logo/light-text.svg') }}" class="h-6 mr-3 sm:h-9 hidden dark:inline" alt="Codemania">
         </a>
 
         <div class="flex md:order-2">
