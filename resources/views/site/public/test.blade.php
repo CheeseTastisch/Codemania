c418daa6e4b42b22638d1ab000bbaec0f230e6c5
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-04-17 19:02:13 +0200

    Added breadcrumb

--- resources/views/site/public/test.blade.php
+++ resources/views/site/public/test.blade.php
@@ -1,16 +1,12 @@
 @extends('layouts.center')
 
 @push('content')
-    <x-alert.x
-        title="Title"
-        sr-info="Information"
-        :style="\App\Models\Components\Styled\Style::Warning">
-        <p>This is an alert</p>
-
-        <x-slot name="actions">
-            <x-alert.actions.link href="{{ url()->previous() }}" :style="\App\Models\Components\Styled\OutlinedStyle::FilledAccent">
-                Zurück
-            </x-alert.actions.link>
-        </x-slot>
-    </x-alert.x>
+<x-breadcrumb.x
+    :items="[
+        ['home' => true, 'name' => 'Projects', 'link' => '#'],
+        ['name' => 'Snap Inc.', 'link' => '#'],
+        ['name' => '2023', 'link' => '#'],
+        ['name' => 'Bitmoji', 'current' => true],
+    ]"
+/>
 @endpush
