6ff58399356adffb6a926bb23a3776e3aa5559e4
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-05-04 20:32:41 +0200

    Reverted join

--- routes/web/member.php
+++ routes/web/member.php
@@ -6,12 +6,3 @@ Route::view('/profile', 'site.member.profile')->name('member.profile');
 
 Route::view('/contests', 'site.member.contest.home')->name('member.contest.home');
 
-Route::get('/join/{contest}', fn(Contest $contest) => setDayAndView(
-    $contest->contestDay,
-    'site.member.contest.join', compact('contest'))
-)->name('member.contest.join');
-
-Route::get('/join/{contest}/team/create', fn(Contest $contest) => setDayAndView(
-    $contest->contestDay,
-    'site.member.contest.team.create', compact('contest'))
-)->name('member.contest.team.create');
