621d4a76b36ac744ca9e67453d14d1401e671541
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-05-16 08:48:02 +0200

    Updated default email texts

---
+++ app/Notifications/test.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class test extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Testing')
+            ->greeting('Hey!')
+            ->line('Hallo, wir testen gerade die Benachrichtigungen.')
+            ->action('Benachrichtigung', url('/'))
+            ->line('Danke für deine Teilnahme!')
+            ->salutation('Dein Codemania-Team');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
