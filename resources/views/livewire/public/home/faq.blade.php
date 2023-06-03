7f07d988feed212267175cb8bf002536188ea103
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-06-03 18:09:14 +0200

    Bugfixes

--- resources/views/livewire/public/home/faq.blade.php
+++ resources/views/livewire/public/home/faq.blade.php
@@ -29,9 +29,9 @@
             <button id="faq-example1-question" aria-controls="faq-example1-answer" :aria-expanded="selected === 'example1'"
                     class="flex items-center justify-between w-full px-4 py-4 font-medium text-lg text-left bg-accent-300 dark:bg-accent-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-accent-600 dark:focus:ring-accent-400"
                     @click="selected = selected === 'example1' ? null : 'example1'">
-                <span class="h-4 w-64 bg-gray-200 rounded-full dark:bg-gray-800 animate-pulse"></span>
+                <span class="h-4 w-64 bg-gray-400 dark:bg-gray-600 rounded-full animate-pulse"></span>
                 <svg class="w-6 h-6 shrink-0 transition-transform duration-500"
-                     :class="{'rotate-180': selected === 'example1"
+                     :class="{'rotate-180': selected === 'example1'}"
                      fill="currentColor"
                      viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
@@ -43,9 +43,9 @@
                 class="px-4 max-h-0 transition-all duration-700 ease-in-out overflow-hidden" x-data
                 x-ref="content" x-bind:style="selected === 'example1' ? 'max-height: ' + $refs.content.scrollHeight + 'px' : ''">
                 <div class="text-sm py-4 font-light animate-pulse">
-                    <div class="h-3 w-80 bg-gray-200 rounded-full dark:bg-gray-800"></div>
-                    <div class="h-3 w-96 bg-gray-200 rounded-full dark:bg-gray-800 mt-2"></div>
-                    <div class="h-3 w-72 bg-gray-200 rounded-full dark:bg-gray-800 mt-2"></div>
+                    <div class="h-3 w-80 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
+                    <div class="h-3 w-96 bg-gray-400 dark:bg-gray-600 rounded-full mt-2"></div>
+                    <div class="h-3 w-72 bg-gray-400 dark:bg-gray-600 rounded-full mt-2"></div>
                     <span class="sr-only">Loading...</span>
                 </div>
             </div>
@@ -54,9 +54,9 @@
             <button id="faq-example2-question" aria-controls="faq-example2-answer" :aria-expanded="selected === 'example2'"
                     class="flex items-center justify-between w-full px-4 py-4 font-medium text-lg text-left bg-accent-300 dark:bg-accent-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-accent-600 dark:focus:ring-accent-400"
                     @click="selected = selected === 'example2' ? null : 'example2'">
-                <span class="h-4 w-64 bg-gray-200 rounded-full dark:bg-gray-800 animate-pulse"></span>
+                <span class="h-4 w-64 bg-gray-400 dark:bg-gray-600 rounded-full animate-pulse"></span>
                 <svg class="w-6 h-6 shrink-0 transition-transform duration-500"
-                     :class="{'rotate-180': selected === 'example2"
+                     :class="{'rotate-180': selected === 'example2'}"
                      fill="currentColor"
                      viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
@@ -68,9 +68,9 @@
                 class="px-4 max-h-0 transition-all duration-700 ease-in-out overflow-hidden" x-data
                 x-ref="content" x-bind:style="selected === 'example2' ? 'max-height: ' + $refs.content.scrollHeight + 'px' : ''">
                 <div class="text-sm py-4 font-light animate-pulse">
-                    <div class="h-3 w-80 bg-gray-200 rounded-full dark:bg-gray-800"></div>
-                    <div class="h-3 w-96 bg-gray-200 rounded-full dark:bg-gray-800 mt-2"></div>
-                    <div class="h-3 w-72 bg-gray-200 rounded-full dark:bg-gray-800 mt-2"></div>
+                    <div class="h-3 w-80 bg-gray-400 dark:bg-gray-600 rounded-full"></div>
+                    <div class="h-3 w-96 bg-gray-400 dark:bg-gray-600 rounded-full mt-2"></div>
+                    <div class="h-3 w-72 bg-gray-400 dark:bg-gray-600 rounded-full mt-2"></div>
                     <span class="sr-only">Loading...</span>
                 </div>
             </div>
