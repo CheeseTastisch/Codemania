c9916572ce7e5a53ebeebf6a9c28609c9501d39a
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-06-03 18:11:18 +0200

    Now initialize swipers over alpine js

--- resources/assets/js/global/swiper.js
+++ resources/assets/js/global/swiper.js
@@ -7,34 +7,3 @@ window.swiper = {
     Swiper: Swiper,
     Autoplay: Autoplay,
 };
-
-Livewire.on('loadSwiper', (id, data) => {
-    const options = {
-        modules: [],
-        direction: data.direction || 'horizontal',
-        loop: data.loop || false,
-        grabCursor: data.grabCursor || false,
-        spaceBetween: data.spaceBetween || 30,
-        slidesPerGroup: data.slidesPerGroup || 1,
-    };
-
-    if (data.breakpoints) {
-        for (const breakpoint in data.breakpoints) {
-            options.breakpoints = {
-                [breakpoint]: {
-                    slidesPerView: data.breakpoints[breakpoint].slidesPerView || 1,
-                }
-            }
-        }
-    }
-
-    if (data.autoplay) {
-        options.modules.push(Autoplay);
-        options.autoplay = {
-            delay: data.autoplay.delay || 3000,
-            disableOnInteraction: data.autoplay.disableOnInteraction || false,
-        }
-    }
-
-    new Swiper(id, options);
-})
