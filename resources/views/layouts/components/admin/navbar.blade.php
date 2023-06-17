de95f675ac2aa7cfb6d039efe8424ca419a588c9
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-06-17 22:20:07 +0200

    Removed dashboard

--- resources/views/layouts/components/admin/navbar.blade.php
+++ resources/views/layouts/components/admin/navbar.blade.php
@@ -25,11 +25,6 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-slate-200 dark:bg-gray-800 md:dark:bg-slate-800 dark:border-gray-700">
                 <li>
-                    <a href="{{ route('admin.dashboard') }}" class="nav-link @if(Route::is('admin.dashboard')) !text-accent-400 dark:!text-accent-600 @endif">
-                        Dashboard
-                    </a>
-                </li>
-                <li>
                     <a href="{{ route('admin.faq') }}" class="nav-link @if(str(Route::currentRouteName())->startsWith('admin.faq')) !text-accent-400 dark:!text-accent-600 @endif">
                         FAQ
                     </a>
--- resources/views/layouts/components/base/user-menu.blade.php
+++ resources/views/layouts/components/base/user-menu.blade.php
@@ -42,7 +42,7 @@
         </li>
         @if(auth()->user()->is_admin)
             <li>
-                <a href="{{ route('admin.dashboard') }}"
+                <a href="{{ route('admin.contest.contest-day.view') }}"
                    class="block px-4 py-2 text-sm text-accent-400 dark:text-accent-600 hover:bg-gray-100 dark:hover:bg-gray-600">
                     Administration
                 </a>
