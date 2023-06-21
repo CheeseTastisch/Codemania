eb1e938bd13829d00d4d925c37254926c89b222b
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-06-21 09:47:57 +0200

    Added discord-link

---
+++ resources/views/site/public/discord.blade.php
@@ -0,0 +1,11 @@
+@php($alwaysDark = true)
+
+@extends('layouts.components.base')
+
+@section('title', 'Discord')
+
+@section('baseContent')
+    <div class="container mx-auto h-screen flex justify-center items-center p-20">
+        <img src="{{ asset('storage/img/discord.png') }}" alt="Discord" class="w-1/2 rounded-3xl">
+    </div>
+@endsection
